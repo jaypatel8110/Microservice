5a7c06620f3d1cbe465410ac742eebf919545531
jaypatel8110 <California@2858>
2020-08-22 20:39:25 -0400

    Pilot to develop

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -15,6 +15,7 @@ public class MovieCatalogServiceApplication {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
+
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
